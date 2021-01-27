028379e7d42eab78979e21445665ce03d9d6c44a
Derek Hammer <hammer@tablexi.com>
2021-01-27 08:48:41 -0700

    Add cerberus and slack channel

--- app/controllers/vaults_controller.rb
+++ app/controllers/vaults_controller.rb
@@ -2,4 +2,12 @@ class VaultsController < ApplicationController
   def show
     render :show
   end
+
+  def update
+    if params[:secret] == 'cerberus'
+      redirect_to '/0b8ee0f6f93c9d6e7d5bb26cafff96acd53b96fd'
+    else
+      redirect_to action: :show
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resource 'secret', only: %w[show update]
 
   get '/853a77881c5eb4733ed5f31b96b10084e9c9525e' => 'vaults#show'
+  patch '/853a77881c5eb4733ed5f31b96b10084e9c9525e' => 'vaults#update'
+
+  get '/0b8ee0f6f93c9d6e7d5bb26cafff96acd53b96fd' => 'cerberus#show'
+
   get '/' => 'countdown#show'
 end
